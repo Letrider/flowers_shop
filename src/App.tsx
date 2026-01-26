257da3348b0495d63e958d06e32bdb4d27248132
MADARAdev <88511184+Letrider@users.noreply.github.com>
2026-01-26 19:26:10 +0300

    fix(): Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { Link, Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
+import { ROUTES } from "./types/routes";
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -9,20 +9,20 @@ import './styles/App.scss';
 const Navigation = () => {
   const location = useLocation();
 
-  const isActive = (path: string) => location.pathname === path;
+  const isActive = (path: ROUTES) => location.pathname === path;
 
   return (
     <nav>
-      <Link to="/" className={isActive('/') ? 'active' : ''}>
+      <Link to="/" className={isActive(ROUTES.home) ? 'active' : ''}>
         Главная
       </Link>
-      <Link to="/products" className={isActive('/products') ? 'active' : ''}>
+      <Link to="/products" className={isActive(ROUTES.products) ? 'active' : ''}>
         Товары
       </Link>
-      <Link to="/about" className={isActive('/about') ? 'active' : ''}>
+      <Link to="/about" className={isActive(ROUTES.about) ? 'active' : ''}>
         О нас
       </Link>
-      <Link to="/contact" className={isActive('/contact') ? 'active' : ''}>
+      <Link to="/contact" className={isActive(ROUTES.contact) ? 'active' : ''}>
         Контакты
       </Link>
     </nav>
---
+++ src/types/routes.ts
@@ -0,0 +1,6 @@
+export enum ROUTES {
+	home = '/',
+	products = '/products',
+	about = '/about',
+	contact = '/contact',
+}
